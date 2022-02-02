a8b08049b9e06e33cb3ebd0b8ff959c8aa4f2481
certara-jcraig <61294078+certara-jcraig@users.noreply.github.com>
2022-02-02 11:42:59 -0800

    add installation functionality for GitHub remote
    additional validation checks and warnings

--- R/install.R
+++ R/install.R
@@ -1,3 +1,6 @@
+#' @export
+JFROG_repo <- "https://certara.jfrog.io/artifactory/certara-cran-release-public/"
+
 #' List all packages developed by Certara
 #'
 #' @param include_self Logical; Set to \code{TRUE} to include \code{Certara.R}
@@ -7,8 +10,9 @@
 #' certara_packages()
 #' @export
 
-certara_packages <- function(include_self= FALSE, include_JFROG_deps = FALSE) {
+certara_packages <- function(include_self= FALSE, include_github = TRUE, include_JFROG_deps = FALSE) {
 
+  # Define character vector of pkgs installed via install.packages()
   pkgs <- c("Certara.RsNLME",
             "Certara.Xpose.NLME",
             "Certara.RsNLME.ModelExecutor",
@@ -16,10 +20,15 @@ certara_packages <- function(include_self= FALSE, include_JFROG_deps = FALSE) {
             "Certara.VPCResults",
             "Certara.RsNLME.ModelBuilder",
             "tidyvpc",
-            "ggquickeda")
+            "ggquickeda",
+            "coveffectsplot")
 
   if(include_self){
     pkgs <- c("Certara.R", pkgs)
+  }
+
+  if(include_github){
+    pkgs <- c(pkgs, names(certara_github_repos()))
   }
 
   if(include_JFROG_deps){
@@ -38,6 +47,11 @@ certara_jfrog_deps <- function() {
   return(pkgs)
 }
 
+certara_github_packages <- function() {
+  list(table1c = "certara/table1c",
+       ggcertara = "certara/ggcertara")
+}
+
 
 
 #' Install Certara R Packages
@@ -45,7 +59,7 @@ certara_jfrog_deps <- function() {
 #' A user friendly wrapper to easily install R packages and Shiny applications developed by Certara
 #'
 #' @param CRAN_repo Character;  The URL of a CRAN mirror which defaults to \code{"https://cloud.r-project.org"}
-#' @param clean_install Logical; Set to \code{TRUE} to remove existing Certara packages prior to installing
+#' @param clean_install Logical; Set to \code{TRUE} to remove all existing Certara packages prior to installing
 #' @param quiet Logical; Set to \code{TRUE} to suppress installation messages and reduce console output
 #'
 #' @return \code{TRUE} if all packages successfully install
@@ -58,10 +72,11 @@ install_certara_packages <- function(CRAN_repo = "https://cloud.r-project.org",
   stopifnot(is_valid_url(CRAN_repo))
 
   if(clean_install){
-    remove_certara_packages()
+    rm_ok <- remove_certara_packages()
+    if(!rm_ok){
+      stop("Cannot remove Certara packages, see below warnings")
+    }
   }
-
-  JFROG_repo <- "https://certara.jfrog.io/artifactory/certara-cran-release-public/"
 
   pkgs_display <- c("None", "All", certara_packages(include_JFROG_deps = FALSE))
 
@@ -74,61 +89,92 @@ install_certara_packages <- function(CRAN_repo = "https://cloud.r-project.org",
   message("The following packages will be installed from ", CRAN_repo,
           " and Certara's package repository ", JFROG_repo, "\n\n",
           "Packages exclusively available on GitHub will be installed using 'remotes::install_github()'",
-          "\n\n", paste(paste(names(pkgs_display), "\t", pkgs_display), collapse = "\n"))
+          "\n\n", paste(paste(names(pkgs_display), "\t", pkgs_display), collapse = "\n"), "\n")
 
   answer <- trimws(readline("Enter number, or an empty line to skip installation: "))
 
+  message("\nInstalling packages...")
+
+  cran_ok <- install_cran_remote(pkgs_display, answer, repos = c(JFROG_repo, CRAN_repo), quiet)
+
+  if(!cran_ok){
+    stop("Installation of packages from CRAN/JFrog remote repository was not successful, see below warnings")
+  }
+
+  github_ok <- install_github_remote(pkgs_display, answer, quiet)
+
+  if(!github_ok){
+    stop("Installation of packages from GitHub remote repository was not successful, see below warnings")
+  }
+}
+
+install_cran_remote <- function(pkgs_display, answer, repos, quiet){
   if(nchar(answer) == 0 || answer == "0"){
-    warning("Installation of Certara packages will not proceed")
+    warning("Installation of Certara packages has been exited by user and will not proceed")
     return(FALSE)
   } else {
     if(answer == "1"){
-      pkgs_to_install <- certara_packages(include_JFROG_deps = TRUE)
-    } else if(answer %in% as.character(2:(length(pkgs_display) - 1))) {
+      pkgs_to_install <- certara_packages(include_JFROG_deps = TRUE, include_github = FALSE)
+    } else if(answer %in% as.character(2:(length(pkgs_display) - 3))) {
       pkgs_to_install <- pkgs_display[names(pkgs_display) == answer]
+    } else if(answer %in% as.character(11:(length(pkgs_display) - 1))) {
+      return(TRUE)
     } else {
       warning("Input entered is invalid, installation of Certara packages will not proceed")
       return(FALSE)
     }
 
-    if(get_os()[[1]] == "windows"){
-      method <- "libcurl"
-    } else {
-      method <- rlang::missing_arg()
-    }
+    install.packages(pkgs_to_install, repos = repos, method = "libcurl", quiet = quiet)
 
-    message("Installing packages...")
-
-    install.packages(pkgs_to_install, repos =c(JFROG_repo, CRAN_repo), method = method, quiet = quiet)
-
-    pkgs_installed <- check_installed_certara_packages()
+    pkgs_installed <- check_installed_certara_packages(certara_packages(include_github = FALSE))
 
     if(!all(pkgs_installed)){
       if(answer == "1"){
-        warning("The following packages did not successfully install: ",
-              paste(names(pkgs_installed)[!pkgs_installed], collapse = "\n"))
+        warning("The following packages from CRAN/JFrog remote did not successfully install:\n",
+                paste(names(pkgs_installed)[!pkgs_installed], collapse = "\n"))
         return(FALSE)
       } else if(!check_installed_certara_packages(pkgs_to_install)){
         warning(pkgs_to_install, " did not successfully install")
         return(FALSE)
       } else {
-        message("Successfully installed", pkgs_to_install)
+        message("Successfully installed the following package from CRAN/JFrog remote:\n", pkgs_to_install)
         return(TRUE)
       }
     } else {
-      message("Successfully installed", pkgs_to_install)
+      message("Successfully installed the following packages from CRAN/JFrog remote:\n", paste(pkgs_to_install, collapse = "\n"))
       return(TRUE)
     }
   }
-}
-
-install_cran_remote <- function(){
 
 }
 
 
-install_github_remote <- function(){
+install_github_remote <- function(pkgs_display, answer, quiet){
 
+  if(answer == "1"){
+    pkgs_to_install <- names(certara_github_packages())
+    lapply(certara_github_packages(), function(x){
+      remotes::install_github(x, upgrade = "never", quiet = quiet)
+    })
+  } else if(answer %in% as.character(11:(length(pkgs_display) - 1))) {
+    pkgs_to_install <- pkgs_display[names(pkgs_display) == answer]
+    repo_to_install <- certara_github_packages()[[pkgs_to_install]]
+    remotes::install_github(repo_to_install,
+                            upgrade = "never", quiet = quiet)
+  } else {
+    return(TRUE)
+  }
+
+  pkgs_installed <- check_installed_certara_packages(pkgs_to_install)
+
+  if(!all(pkgs_installed)){
+    warning("The following packages from GitHub remote did not successfully install:\n",
+              paste(names(pkgs_installed)[!pkgs_installed], collapse = "\n"))
+    return(FALSE)
+  } else {
+    message("Successfully installed the following packages from GitHub remote:\n", paste(pkgs_to_install, collapse = "\n"))
+    return(TRUE)
+  }
 }
 
 #' Remove Certara R Packages
@@ -149,14 +195,24 @@ remove_certara_packages <- function(){
         silent = TRUE)
   }
 
-  installed.Certara.pkgs <-
+  find_packages <- function(pkgs){
     basename(find.package(pkgs, quiet = TRUE))
-
-  if(length(installed.Certara.pkgs > 0)) {
-    remove.packages(installed.Certara.pkgs)
   }
 
-  return(TRUE)
+  installed.Certara.pkgs <- find_packages(pkgs)
+
+  if(length(installed.Certara.pkgs) > 0) {
+    remove.packages(installed.Certara.pkgs)
+    pkgs_not_removed <- find_packages(pkgs)
+    if(length(pkgs_not_removed) > 0){
+      warning("Cannot remove the following package(s):\n", paste(pkgs_not_removed, collapse = "\n"))
+      return(FALSE)
+    } else {
+      return(TRUE)
+    }
+  } else {
+    return(TRUE)
+  }
 }
 
 #' Check if Certara packages are installed
@@ -185,41 +241,27 @@ check_installed_certara_packages <- function(pkgs){
 
 #' Check if Certara packages are installed
 #'
-#' @return Named logical vector indicating whether Certara package is installed
+#' @return Named character vector indicating package version, \code{NA} is returned if Certara package is not installed
 #' @examples
-#' check_installed_certara_packages()
+#' check_certara_package_versions()
 #' @export
 
-check_certara_package_versions <- function(){
+check_certara_package_versions <- function(pkgs){
 
-  pkgs <- certara_packages(include_JFROG_deps = TRUE)
+  if(missing(pkgs)){
+    pkgs <- certara_packages(include_self = TRUE, include_JFROG_deps = TRUE)
+  }
 
   pkgs_version <- sapply(pkgs, function(x)
     if(system.file(package = x) == ""){
       return(NA)
     } else {
-      return(packageVersion(x))
+      return(as.character(packageVersion(x)))
     })
 
   return(pkgs_version)
 }
 
-
-get_os <- function(){
-  sysinf <- Sys.info()
-  if (!is.null(sysinf)){
-    os <- sysinf['sysname']
-    if (os == 'Darwin')
-      os <- "osx"
-  } else { ## mystery machine
-    os <- .Platform$OS.type
-    if (grepl("^darwin", R.version$os))
-      os <- "osx"
-    if (grepl("linux-gnu", R.version$os))
-      os <- "linux"
-  }
-  return(tolower(os))
-}
 
 is_valid_url <- function(url, timeout=2){
 
